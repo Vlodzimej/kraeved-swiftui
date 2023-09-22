d6466aa5401103edc69d88971be1aa8e968f9001
Vladimir Amelkin <vamelkin@vbankcenter.ru>
2023-09-22 19:32:16 +0300

    Initial Commit

---
+++ kraeved/kraevedApp.swift
@@ -0,0 +1,17 @@
+//
+//  kraevedApp.swift
+//  kraeved
+//
+//  Created by Владимир Амелькин on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct kraevedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
